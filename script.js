7c896cf849a11ba84dae94274e42736e48371932
Kingsley Chijioke <kumaseyin4@gmail.com>
2020-12-12 06:40:21 +0100

    button seperation

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ const updateVisualState = (key, calculator) => {
   Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
 
   if (keyType === 'operator') key.classList.add('is-depressed')
-  if (keyType === 'clear' && key.textContent !== 'AC') key.textContent = 'AC'
+  if (keyType === 'clear' && key.textContent !== 'AC')
   if (keyType !== 'clear') {
     const clearButton = calculator.querySelector('[data-action=clear]')
     clearButton.textContent = 'C'
